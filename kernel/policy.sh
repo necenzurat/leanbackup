efacabdf2530f5d02e142a92c1d9825a912277e1
Costin Moise <necenzurat@gmail.com>
2019-01-13 20:22:06 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ kernel/policy.sh
@@ -0,0 +1,25 @@
+{
+    for backupPolicy in $(ls ${location}/backups/$repository/backup/*); do 
+        logme "[$backupName] Doing backup policy: $backupPolicy";
+        include $backupPolicy
+        
+        # errorLines=$(wc -l < $errorLogsFile);
+        # if [ "$errorLines" -gt "0" ]; then
+        #     errorMessage=$(cat $errorLogsFile)
+        #     slack "$errorMessage\n\nError occured: [$backupName] - Backup policy: $backupPolicy"
+        # fi
+        # unset -v errorLines
+        # unset -v errorMessage
+    done
+
+    for syncPolicy in $(ls ${location}/backups/$repository/sync/*); do 
+        logme "[$backupName] Doing sync policy: $syncPolicy";
+        include $syncPolicy  
+    done
+
+    for cleanup in $(ls ${location}/backups/$repository/cleanup/*); do 
+        logme "[$backupName] Doing cleanup policy: $cleanup";
+        include $cleanup 
+    done
+} 2>> $errorLogsFile
+
