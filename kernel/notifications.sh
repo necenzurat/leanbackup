6d5592ad40026f31e3bc4cb0b7f3b55154e6627d
Costin Moise <necenzurat@gmail.com>
2019-01-14 02:28:28 +0200

    wip

--- kernel/notifications.sh
+++ kernel/notifications.sh
@@ -1,14 +1,14 @@
-#errorLines=$(wc -l < $errorLogsFile);
+errorLines=$(wc -l < $errorLogsFile);
 
-# if [ "$errorLines" -gt "0" ]; then
-#     errorContents=$(cat $errorLogsFile)
-#     #prowl "Backup run, took $runTime ⌚";
-#     slack "an error occured\n$errorContents";
-# else 
-#     rm -rf $errorLogsFile;
-# fi
+if [ "$errorLines" -gt "0" ]; then
+    errorContents=$(cat $errorLogsFile)
+    #prowl "Backup run, took $runTime ⌚";
+    slack "An error occured\n$errorContents";
+else 
+    rm -rf $errorLogsFile;
+fi
 
-# if [ ! -z "$notifyWhenBackupsAreDone" ]; then
-#     prowl "Backup run, took $runTime ⌚";
-#     slack "Backup run, took $runTime ⌚";
-# fi
+if [[ "$notifyWhenBackupsAreDone" == "true" ]]; then
+    #prowl "Backup run, took $runTime ⌚";
+    slack "Backup run, took $runTime ⌚";
+fi
