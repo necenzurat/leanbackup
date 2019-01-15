b26b6006b2480bb6e9683fc5ebd9417352d908d8
Costin Moise <necenzurat@gmail.com>
2019-01-15 13:42:23 +0200

    wip

--- kernel/policies/policy.sh
+++ kernel/policies/policy.sh
@@ -3,7 +3,6 @@ set -o allexport
 include ${location}/backups/$repository/.env
 set +o allexport
 
-echo $RESTIC_REPOSITORY
 if [[ "$skipPolicies" == "true" ]]; then
     logme "[$backupName] manually skipping time policies"
     include ${location}/kernel/policies/backup.sh
